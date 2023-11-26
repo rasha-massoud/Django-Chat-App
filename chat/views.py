2b642cc4454a9b72b9b53131799cc3cd86921617
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-26 14:29:56 +0200

    Created the function to the room path

--- chat/views.py
+++ chat/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def room(request, room):
+    return render(request, 'room.html', {'room': room})
