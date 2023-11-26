8401713fdfcf025f6ded0189bf9d3c905d5dcb07
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-26 14:30:40 +0200

    Created the path to the checkview function

--- chat/urls.py
+++ chat/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('<str:room>/', views.room, name='room'),
+    path('checkview', views.checkview, name='checkview'),
 ]
